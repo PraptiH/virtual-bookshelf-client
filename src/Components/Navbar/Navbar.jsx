a486d2cd4c9554590cc28983695cadf6e935df69
PraptiH <praptiantara1892@gmail.com>
2025-10-11 13:05:37 +0600

    Add navbar and footer, apply DaisyUI cupcake theme

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../../assets/logo.jpg'
+import { NavLink } from 'react-router';
+const Navbar = () => {
+
+    return (
+        <div className='mt-10 w-11/12 mx-auto flex items-center justify-between'>
+            <div className='flex items-center gap-5'>
+                <img className='rounded-full w-30' src={logo} alt="" />
+                <h3 className='font-bold text-3xl '>Digital Boipoka</h3>
+            </div>
+            <div className='flex gap-5'>
+                <NavLink to="/" className={({isActive})=>isActive? 'text-primary' : 'text-base-content hover:text-primary'}>Home</NavLink>
+                <NavLink to="/bookshelf" className={({isActive})=>isActive? 'text-blue-500' : 'text-gray-500'}>Bookshelf</NavLink>
+            </div>
+
+            <div>
+                <button className='btn btn-primary font-medium text-lg p-5'>Log In</button>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../Layouts/RootLayout/RootLayout";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:RootLayout,
+    children:[
+        {
+            index:true,
+            path:'/',
+            Component:Home
+        }
+    ]
+  },
+]);
